a4aa9ea2420f878853b60a8795097b64e3facfc8
ats1999 <rkt82224@gmail.com>
2024-03-02 13:46:39 +0530

    init meta server

---
+++ meta-server/src/main/java/com/watchman/metaserver/MetaServerApplication.java
@@ -0,0 +1,13 @@
+package com.watchman.metaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaServerApplication.class, args);
+	}
+
+}
---
+++ meta-server/src/test/java/com/watchman/metaserver/MetaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watchman.metaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
