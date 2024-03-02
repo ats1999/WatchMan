7ee05fc83ddcfdb9a1a09341a1b376b062e95ab8
ats1999 <rkt82224@gmail.com>
2024-03-02 22:49:54 +0530

    added HandlerMethodValidationException exception handler

--- meta-server/src/main/java/com/watchman/metaserver/config/RestResponseExceptionHandler.java
+++ meta-server/src/main/java/com/watchman/metaserver/config/RestResponseExceptionHandler.java
@@ -1,14 +1,28 @@
 package com.watchman.metaserver.config;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+ @ControllerAdvice
 public class RestResponseExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(ResponseStatusException.class)
   public String handleResponseStatusException(ResponseStatusException ex) {
     return ex.getReason();
+  }
+
+  @Override
+  public ResponseEntity<Object> handleHandlerMethodValidationException(
+      HandlerMethodValidationException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+      return ResponseEntity.badRequest().body(ex.getDetailMessageArguments());
   }
 }
